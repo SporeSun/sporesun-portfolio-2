0f081b57f292ffb2eff2e14b324696c0b6d43851
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-11-13 18:24:48 -0800

    completed Contact.jsx

---
+++ src/components/Contact.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const Contact = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    });
+
+    const handleChange = (event) => {
+        setFormData({
+            ...formData,
+            [event.target.name]: event.target.value
+        });
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Form submission logic
+    };
+
+    return (
+        <section>
+            <h2>Contact</h2>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder="Name" onChange={handleChange} required />
+                <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
+                <textarea name="message" placeholder="Message" onChange={handleChange} required></textarea>
+                <button type="submit">Send</button>
+            </form>
+        </section>
+    );
+};
+
+export default Contact;
