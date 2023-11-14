65790fe7ed75da4617ffb7b02f3c04ddf9c606ca
SporeSun <135516150+SporeSun@users.noreply.github.com>
2023-11-13 18:23:33 -0800

    completed AboutMe.jsx

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <section>
+            <h2>About Me</h2>
+            <img src="path-to-your-image.jpg" alt="Developer" />
+            <p>Your bio here...</p>
+        </section>
+    );
+};
+
+export default AboutMe;
